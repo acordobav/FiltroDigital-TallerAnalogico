805f7bdafef919c4050947f229a7d3515c60570f
Arturo Cordoba <arturocv16@gmail.com>
2020-12-14 02:25:10 -0600

    Multiplicacion funcional

--- src/Multiplication.sv
+++ src/Multiplication.sv
@@ -1,16 +1,14 @@
 module Multiplication #(parameter N = 16)
 							  (input logic  [N-1:0] A, B,
 							   output logic [N-1:0] Result);
-logic neg;
-								
+logic neg, overflow, overflow_result;					
 logic [N-1:0] Am, Bm;
-								
-assign Am = A[N-1] ? (~A)+1 : A; // Complemento a 2
-assign Bm = B[N-1] ? (~B)+1 : B; // Complemento a 2
-								
-logic [(N*2)-1:0] result;				
-logic [N-1:0] high, mid, low; 							
+logic [(N*2)-1:0] mult;		
+logic [N-1:0] high, mid, low;					
 logic [(N/2)-1:0] a, b, c, d;
+								
+assign Am = A[N-1] ? (~A)+1 : A; // Complemento a 2 si numero es negativo
+assign Bm = B[N-1] ? (~B)+1 : B; // Complemento a 2 si numero es negativo
 
 assign a = Am[N-1:(N/2)];
 assign b = Am[(N/2)-1:0];
@@ -21,14 +19,16 @@ assign high = (a * c);
 assign mid  = (a * d) + (b * c);
 assign low  = (b * d); 
 
-assign result = (high << (N/2)) + mid + (low >> (N/2));
+// Calcular resultado de la multiplicacion
+assign mult = ((high << (N/2)) + mid + (low >> (N/2)));
 
+// Determinar si resultado final debe ser negativo o positivo
 assign neg = (A[N-1] || B[N-1]) && (A[N-1] !== B[N-1]); 
 
 always_comb begin
-	if(result[(N*2)-1:N] === {{N{1'b0}}}) begin
-		// No existe overflow
-		Result <= neg ? (~result[N-1:0])+1 : result[N-1:0];
+	if(mult[(N*2)-1:N-1] === {{N{1'b0}}}) begin
+		// No existe overflowd
+		Result <= neg ? (~mult[N-1:0])+1 : mult[N-1:0];
 	end else begin
 		// Existe overflow
 		Result <= neg ? ({1'b1, {N-2{1'b0}}, 1'b1}) : {1'b0, {N-1{1'b1}}};
--- src/Multiplication_tb.sv
+++ src/Multiplication_tb.sv
@@ -1,62 +1,93 @@
 module Multiplication_tb();
 		
-logic [15:0] A, B, Result;
+logic [31:0] A, B, Result;
 	
-Multiplication #(16) mult(A, B, Result);
+Multiplication #(32) mult(A, B, Result);
 
 initial begin
-	// Caso 1
-	A = 16'b0000000110000000;
-	B = 16'b0000001101000000;
+	// Dos numeros positivos
+	// Caso 1: 1.5 * 3.25 = 4.875
+	A = 32'h18000;
+	B = 32'h34000;
 	#10;
-	assert (Result === 16'b0000010011100000) else $error("Case 1: Result failed.");
-	#10;
-	
-	// Caso 2
-	A = 16'b1111111001111111;
-	B = 16'b0000001101000000;
-	#10;
-	assert (Result === 16'h1C001) else $error("Case 2: Result failed.");
+	assert (Result === 32'h4E000) else $error("Case 1: Result failed.");
 	#10;
 	
-	// Caso 3
-	A = 16'b0111111110000000;
-	B = 16'b0000010101000000;
+	// Caso 2: 5.125 * 6.625 = 33.953125
+	A = 32'h52000;
+	B = 32'h6A000;
 	#10;
-	assert (Result === 16'b0111111111111111) else $error("Case 3: Result failed.");
-	#10;
-	
-	// Caso 4
-	B = 16'b1111111010000000;
-	A = 16'b0000001101000000;
-	#10;
-	assert (Result === 16'b1111101111100000) else $error("Case 4: Result failed.");
-	#10;
-
-/*		
-logic [15:0] A, B, Result;
-	
-Multiplication #(16) mult(A, B, Result);
-
-initial begin
-	
-	// Caso 1
-	A = 32'h00060C2F;
-	B = 32'h0002009A;
-	#10;
-	assert (Result === 32'h000003A3) else $error("Case 1: Result failed.");
+	assert (Result === 32'h21F400) else $error("Case 2: Result failed.");
 	#10;
 	
-	// Caso 2
-	A = 32'hFFFFFECB;
-	B = 32'h00060C2F;
+	// Numero positivo y negativo
+	// Caso 3: -10.5 * 35.3125
+	A = 32'hFFF58000;
+	B = 32'h235000;
 	#10;
-	assert (Result === 32'hFFF904E2) else $error("Case 2: Result failed.");
-	#10;*/
+	assert (Result === 32'hFE8D3800) else $error("Case 3: Result failed.");
+	#10;
 	
+	// Caso 4: 60.125 * -501.5 = -30152.6875
+	A = 32'h3C2000;
+	B = 32'hFE0A8000;
+	#10;
+	assert (Result === 32'h8A375000) else $error("Case 4: Result failed.");
+	#10;
+	
+	// Caso overflow
+	// Caso 5: 7500 * 8 = 60000 (bit 32 es un 1)
+	A = 32'h1D4C0000;
+	B = 32'h80000;
+	#10;
+	assert (Result === 32'h7FFFFFFF) else $error("Case 5: Result failed.");
+	#10;
+	
+	// Caso 6: 15 * 15000 = 225000 (bit 32 es un 1)
+	A = 32'hF0000;
+	B = 32'h3A980000;
+	#10;
+	assert (Result === 32'h7FFFFFFF) else $error("Case 6: Result failed.");
+	#10;
+	
+	// Caso underflow
+	// Caso 7: 15 * -15000 = -225000 (bit 32 es un 1)
+	A = 32'hF0000;
+	B = 32'hC5680000;
+	#10;
+	assert (Result === 32'h80000001) else $error("Case 7: Result failed.");
+	#10;
+	
+	// Caso 8: 7500 * -8 = -60000 (bit 32 es un 1)
+	A = 32'h1D4C0000;
+	B = 32'hFFF80000;
+	#10;
+	assert (Result === 32'h80000001) else $error("Case 8: Result failed.");
+	#10;
+	
+	// Multiplicacion dos numeros negativos
+	// Caso 9: -545.0125 * -15.5 = 8447.69375
+	A = 32'hFDDEFCCD;
+	B = 32'hFFF08000;
+	#10;
+	assert (Result === 32'h20FFB196) else $error("Case 9: Result failed.");
+	#10;
+	
+	// Multiplicacion por cero
+	// Caso 10: positivo por cero -> 3.25 * 0 = 0
+	A = 32'h34000;
+	B = 32'h0;
+	#10;
+	assert (Result === 32'h0) else $error("Case 10: Result failed.");
+	#10;
+	
+	// Caso 11: negativo por cero -> 0 * -545.0125 = 0
+	A = 32'h0;
+	B = 32'hFDDEFCCD;;
+	#10;
+	assert (Result === 32'h0) else $error("Case 11: Result failed.");
+	#10;
 	
 end
-
-
 
 endmodule 
